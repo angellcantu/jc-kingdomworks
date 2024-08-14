c6386564eb02385fe1c88003f56ef114cd78156b
JOSE ANGEL CANTU ALCOCER <jacantual@gmail.com>
2024-08-14 12:27:20 -0600

    New service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
+'use strict';
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
+	getHello(): string {
+		return 'Hello World!';
+	}
+
+	getJSON(): { success: boolean, message: string } {
+		return {
+			success: true,
+			message: 'Hello!!'
+		};
+	}
+
 }
